dd3c23d07c9902f976e5c3ce62e5a90f9d3d6507
Michael Nnaji <mnnaji3@uwo.ca>
2021-06-14 20:53:14 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,8 @@ class SessionsController < ApplicationController
 
 
   def create
-    @user = User.find_by(name: params[:name])
-    if @user && @user.authenticate(params[:password])
+    @user = User.find_by(name: user_params[:name])
+    if @user && @user.authenticate(user_params[:password])
       session[:user_id] = @user.id
     else
         return head(:forbidden)
