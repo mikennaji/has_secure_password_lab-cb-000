2be6aeaa561319433c091b67c82daf79b2daffdc
Michael Nnaji <mnnaji3@uwo.ca>
2021-06-14 20:28:11 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
     else
-        return head(:forbidden) unless @user.authenticate(params[:password])
+        return head(:forbidden) 
   end
 
 end
