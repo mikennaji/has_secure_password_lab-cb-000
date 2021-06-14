d31f9c5beb0bc7f9ea23388537dfc7f0b583b9e4
Michael Nnaji <mnnaji3@uwo.ca>
2021-06-14 20:31:32 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
 
   def create
     if params[:password] != params[:password_confirmation]
-      render :new 
-    else 
+      render :new
+    else
       User.create(user_params)
 
   end
@@ -11,7 +11,7 @@ end
 
 private
 
-def user_params 
+def user_params
   params.require(:user).permit(:name, :password, :password_confirmation)
 end
 
